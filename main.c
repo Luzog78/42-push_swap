81c1b3db13bce11c3380115886b452d472c2c75a
Yanis Sabik <ysabik@student.42angouleme.fr>
2023-11-07 14:32:54 +0100

    Initial commit

--- main.c
+++ main.c
@@ -6,19 +6,70 @@
 /*   By: ysabik <ysabik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 08:53:09 by ysabik            #+#    #+#             */
-/*   Updated: 2023/11/05 11:11:22 by ysabik           ###   ########.fr       */
+/*   Updated: 2023/11/05 16:53:54 by ysabik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	ft_init_data(t_data *data)
+{
+	data->current_floor = NULL;
+	data->history = NULL;
+}
+
+void	ft_add_front_pos(t_pos **pos, t_pos *new)
+{
+	if (!*pos)
+		*pos = new;
+	else
+	{
+		new->next = *pos;
+		*pos = new;
+	}
+}
+
+int	ft_stack_equals(t_stack *stack1, t_stack *stack2)
+{
+	while (stack1 && stack2)
+	{
+		if (stack1->value != stack2->value)
+			return (0);
+		stack1 = stack1->next;
+		stack2 = stack2->next;
+	}
+	if (stack1 || stack2)
+		return (0);
+	return (1);
+}
+
+int	ft_pos_equals(t_pos *pos1, t_pos *pos2)
+{
+	return (ft_stack_equals(pos1->stack_a, pos2->stack_a)
+		&& ft_stack_equals(pos1->stack_b, pos2->stack_b));
+}
+
+void	ft_add_to_next_floor(t_data *data, t_pos *from)
+{
+	t_pos	*tmp;
+
+	if ()
+}
+
+void	ft_next_floor(t_data *data)
+{
+	t_pos	*next_floor;
+	
+	
+}
+
 int	main(int argc, char **argv)
 {
-	t_stack	*stack_a;
-	t_stack	*stack_b;
+	t_data	data;
 
-	stack_a = NULL;
-	stack_b = NULL;
+	if (argc < 2)
+		return (0);
+	ft_init_data(&data);
 	
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,24 +6,53 @@
 /*   By: ysabik <ysabik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 08:53:04 by ysabik            #+#    #+#             */
-/*   Updated: 2023/11/05 09:01:06 by ysabik           ###   ########.fr       */
+/*   Updated: 2023/11/05 16:41:08 by ysabik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include <stdlib.h>
+
+typedef enum e_move_type
+{
+	SA,
+	SB,
+	SS,
+	PA,
+	PB,
+	RA,
+	RB,
+	RR,
+	RRA,
+	RRB,
+	RRR
+}	t_move_type;
+
+typedef struct s_move
+{
+	t_move_type		type;
+	struct s_move	*next;
+}	t_move;
 
 typedef struct s_stack
 {
-	int				n;
+	int				value;
 	struct s_stack	*next;
 }	t_stack;
 
 typedef struct s_pos
 {
-	t_stack	*stack_a;
-	t_stack	*stack_b;
+	t_stack			*stack_a;
+	t_stack			*stack_b;
+	t_move			*moves;
+	struct s_pos	*next;
 }	t_pos;
 
+typedef struct s_data
+{
+	t_pos	*current_floor;
+	t_pos	*history;
+}	t_data;
 
 #endif
